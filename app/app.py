3e3e39827feb444f76580b4757adbdeafa51a8c3
Ayushi Agarwal <84909280+ayushiagarawal1@users.noreply.github.com>
2023-09-04 19:33:22 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -89,19 +89,19 @@ def index():
 
 @app.route("/generate-sop", methods=["POST"])
 def generate_sop():
-    prompt = request.json
+    user_inputs = request.json
 
     # Generate statement of purpose
-    #prompt = f"I am applying for Student Visa in Canada. I have provided you with the details now generate me a statement of purpose. Deatils are: \n Dear Admissions Committee,\nI am writing this statement of purpose to express my interest in {user_inputs['admitted_program']}...\n{user_inputs['name']} {user_inputs['age']}...\n i completed my highest level of education {user_inputs['selectedEducation']} from {user_inputs['past_institute']} and i studied {user_inputs['program']} there. And i am from {user_inputs['country']} country. \n My future goals are: {user_inputs['future_goals']} \n  I have work experience of {user_inputs['work_experience']} \n Now i want to pursue higher level of education from Canada and i have been admitted to {user_inputs['institute']} in program- {user_inputs['admitted_program']} and the status of my paid tution fee for first year is {user_inputs['selectedTuitionFee']} and i have already paid {user_inputs['paid_fees']}\n The status of my gic is {user_inputs['selectedGicFee']} and i have paid amount of {user_inputs['paid_gic']} \n My proficiency in english is shown by my scores. I scored {user_inputs['listening_score']} in English-listening, {user_inputs['reading_score']} in English Reading, {user_inputs['writing_score']} in English Writing and {user_inputs['speaking_score']} in English speaking\n \n I want you to follow a general template for statement of purpose. Make my skills show and try to gain a acceptable response from the visa officer. Give me this template in different sections- 1. From and To section From must include the name, sample address, city, state, zip code and email. Whereas To contains To, Visa Officer, High Commission of Canada, City, State, Zip code \n 2. Subject-statement of purpose for studying in canada \n 3. Introduction 4.Why the Program of Study in Canada at the Education Institute Name \n 5.Why Study in Canada: \n 6.My Future Goals: \n 7.Academic Background and Language Proficiency: \n 8. Finances: include the point of my paid fees already and gic \n 9. Conclusion: \n 10. Greetings with name {user_inputs['name']}. \n Give every paragraph in minimum 300 words."
+    prompt = f"I am applying for Student Visa in Canada. I have provided you with the details now generate me a statement of purpose. Deatils are: \n Dear Admissions Committee,\nI am writing this statement of purpose to express my interest in {user_inputs['admitted_program']}...\n{user_inputs['name']} {user_inputs['age']}...\n i completed my highest level of education {user_inputs['selectedEducation']} from {user_inputs['past_institute']} and i studied {user_inputs['program']} there. And i am from {user_inputs['country']} country. \n My future goals are: {user_inputs['future_goals']} \n  I have work experience of {user_inputs['work_experience']} \n Now i want to pursue higher level of education from Canada and i have been admitted to {user_inputs['institute']} in program- {user_inputs['admitted_program']} and the status of my paid tution fee for first year is {user_inputs['selectedTuitionFee']} and i have already paid {user_inputs['paid_fees']}\n The status of my gic is {user_inputs['selectedGicFee']} and i have paid amount of {user_inputs['paid_gic']} \n My proficiency in english is shown by my scores. I scored {user_inputs['listening_score']} in English-listening, {user_inputs['reading_score']} in English Reading, {user_inputs['writing_score']} in English Writing and {user_inputs['speaking_score']} in English speaking\n \n I want you to follow a general template for statement of purpose. Make my skills show and try to gain a acceptable response from the visa officer. Give me this template in different sections- 1. From and To section From must include the name, sample address, city, state, zip code and email. Whereas To contains To, Visa Officer, High Commission of Canada, City, State, Zip code \n 2. Subject-statement of purpose for studying in canada \n 3. Introduction 4.Why the Program of Study in Canada at the Education Institute Name \n 5.Why Study in Canada: \n 6.My Future Goals: \n 7.Academic Background and Language Proficiency: \n 8. Finances: include the point of my paid fees already and gic \n 9. Conclusion: \n 10. Greetings with name {user_inputs['name']}. \n Give every paragraph in minimum 300 words."
     generated_sop = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=500  # Adjust as needed
+        max_tokens=1800  # Adjust as needed
     ).choices[0].text.strip()
 
     # Send email
-    from_email = email_from
-    from_password = email_from_pass
+    from_email = 'agarawal.ayushi1.work@gmail.com'
+    from_password = 'tlnapgmmlhknkepl'
     to_email = user_inputs["email"]
 
     subject = "Statement of Purpose"
